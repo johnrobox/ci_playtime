589cf6a7c3ebbf0c49ae768baef6d2a1b1aef96a
John Robert Jerodiaz (Roy) <royfdc@gmail.com>
2017-10-24 14:15:10 +0800

    qr code

--- application/controllers/QrcodeController.php
+++ application/controllers/QrcodeController.php
@@ -1,5 +1,7 @@
 <?php
 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 // SOURCE : https://github.com/dwisetiyadi/CodeIgniter-PHP-QR-Code
 class QrcodeController extends CI_Controller {
 
@@ -9,11 +11,29 @@ class QrcodeController extends CI_Controller {
         $params['data'] = 'This is a text to encode become QR Code';
         $params['level'] = 'H';
         $params['size'] = 10;
-        $params['savename'] = 'http://192.168.0.117:3333/images/tes.png';
+        $params['savename'] = FCPATH.'images/tes.png';
         $this->ciqrcode->generate($params);
 
-        echo '<img src="tes.png" />s';
+        echo '<img src="'.base_url().'images/tes.png" />';
     }
-}
 
- ?>
+    public function qrOptionalConfiguration() {
+        $this->load->library('ciqrcode');
+        $config['cacheable']	= true; //boolean, the default is true
+        $config['cachedir']		= ''; //string, the default is application/cache/
+        $config['errorlog']		= ''; //string, the default is application/logs/
+        $config['quality']		= true; //boolean, the default is true
+        $config['size']			= ''; //interger, the default is 1024
+        $config['black']		= array(224,255,255); // array, default is array(255,255,255)
+        $config['white']		= array(70,130,180); // array, default is array(0,0,0)
+        $this->ciqrcode->initialize($config);
+        $params['data'] = 'This is a text to encode become QR Code';
+        $params['level'] = 'H';
+        $params['size'] = 10;
+        $params['savename'] = FCPATH.'images/tes1.png';
+        $this->ciqrcode->generate($params);
+
+        echo '<img src="'.base_url().'images/tes1.png" />';
+    }
+
+}
